ebb28c447f415e1acb201e1e69e88cd23bf9af72
David Mojica <davidmojicav@gmail.com>
2024-04-08 15:54:01 -0500

    Completo - Funcional

--- app/src/main/java/com/etcetera/quotology/QuoteActivity.java
+++ app/src/main/java/com/etcetera/quotology/QuoteActivity.java
@@ -1,15 +1,31 @@
 package com.etcetera.quotology;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuoteActivity extends AppCompatActivity {
-    Quote quote;
+    TextView Autor, Quote;
+    String Autor_str, Quote_str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         //Crear el xml de quote activity
+        setContentView(R.layout.quote_activity);
+
+        Autor = findViewById(R.id.autor_text);
+        Quote = findViewById(R.id.quote_text);
+
+        Intent get_quote = this.getIntent();
+        if (get_quote != null){
+            Autor_str = get_quote.getStringExtra("quote_author");
+            Quote_str = get_quote.getStringExtra("quote");
+
+            Autor.setText(Autor_str);
+            Quote.setText(Quote_str);
+        }
     }
 }
